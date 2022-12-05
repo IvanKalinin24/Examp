fb2b1b6e28d78deb6d42cbc4448e19681fdedf91
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-05 23:36:02 +0700

    Задача 8 исправление

--- DZ_Task8/Program.cs
+++ DZ_Task8/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.Clear();
-Console.Write("Введите число ");
+Console.Write("Введите число: ");
 
 int N = int.Parse(Console.ReadLine());
 int B = 1;
 
-while (B <= N); 
-    if (B % 2 == 0);
-    
-Console.WriteLine("{0} ", B);
-B++;
+while (B <= N)
+{ 
+Console.Write(B+1);
+B+=2;
+}
