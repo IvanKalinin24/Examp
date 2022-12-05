2e9aa9546f2a9ae62c46fe8620de79842d301112
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-05 17:36:41 +0700

    Задача 4

--- DZ_Task4/Program.cs
+++ DZ_Task4/Program.cs
@@ -1,4 +1,5 @@
-﻿int num1 = new Random().Next(1, 10);
+﻿Console.Clear();
+int num1 = new Random().Next(1, 10);
 Console.WriteLine(num1);
 int num2 = new Random().Next(1, 10);
 Console.WriteLine(num2);
