1ee44b922a700b78c1ab4b223d3f02d2d8dc9500
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-05 18:18:57 +0700

    Задача 6

---
+++ DZ_Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int i;
+Console.Write("Ввидите число ");
+i = int.Parse(Console.ReadLine());
+
+    if(i % 2 == 0){
+        Console.Write("Введеное число считается четным ");
+        Console.Read();
+    }
+      
+    else{
+        Console.Write("Введеное число нечетное ");
+        Console.Read();
+    }
+
+
+
+
+
