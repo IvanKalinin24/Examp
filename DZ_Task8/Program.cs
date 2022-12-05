e4974db6fd8bbd55107ceb3ccfaa3c3cb8990377
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-05 22:44:41 +0700

    Задача 8

---
+++ DZ_Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+
+int N = int.Parse(Console.ReadLine());
+int B = 1;
+
+while (B <= N); 
+    if (B % 2 == 0);
+    
+Console.WriteLine("{0} ", B);
+B++;
