182c9e4a33c8fcd61e0344ec79d465f3a8dcd0c8
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-05 13:54:30 +0700

    Задача 4 пробный вариант

---
+++ DZ_Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1 = new Random().Next(1, 10);
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
+int num3 = new Random().Next(1, 10);
+Console.WriteLine(num3); 
+int max = num1;
+
+if(num1 > max) max = num1;
+if(num2 > max) max = num2;
+
+Console.Write("max = ");
+Console.WriteLine(max); 
