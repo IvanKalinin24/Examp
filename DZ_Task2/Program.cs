61a4454247aa7c679119cf5bf1c47bb543f09f40
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-05 13:44:31 +0700

    Задача 2 Исправлени

--- DZ_Task2/Program.cs
+++ DZ_Task2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int num1 = new Random().Next(1, 10);
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
+int max = num1;
+
+if(num1 > max) max = num1;
+if(num2 > max) max = num2;
+
+Console.Write("max = ");
+Console.WriteLine(max); 
