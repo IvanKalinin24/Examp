2a1d4ea227e995679337062b875fda6c265386d3
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-08 00:56:29 +0700

    Степень

---
+++ DZ_Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.WriteLine("Ввидите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввидите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+int result = 1;
+for (i = 1; i <= numberB; i++)
+{
+    result = result * numberA;
+}
+
+Console.WriteLine($"Ответ {result}");
