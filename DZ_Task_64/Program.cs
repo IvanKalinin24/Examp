395a77a49eda8674e4c808652da081974816de00
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-16 23:39:48 +0700

    Task64

---
+++ DZ_Task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+
+Clear();
+
+Clear();
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+int m=1;
+
+WriteLine(PrintNumbers(m,n));
+
+string PrintNumbers(int start, int end)
+{
+    if (end == start) 
+        return end.ToString();
+    return (PrintNumbers(start + 1, end)+ " " + start);
+}
