5b7241c7559a022faf15d4b7eed9daea33a7370a
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-17 22:22:03 +0700

    Task66

---
+++ DZ_Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+
+Clear();
+Write("Введите M: ");
+int m=int.Parse(ReadLine());
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+
+WriteLine(PrintSum(m,n));
+
+int PrintSum(int m, int n)
+{
+    if (m == 0) return (n * (n + 1)) / 2;            
+    else if (n == 0) return (m * (m + 1)) / 2;       
+    else if (m == n) return m;                       
+    else if (m < n) return n + PrintSum(m, n - 1); 
+    else return n + PrintSum(m, n + 1);            
+}
