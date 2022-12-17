7e99f71f889cb4173f25831aba52c0159f0b9574
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-17 22:57:02 +0700

    Task68

---
+++ DZ_Task68/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+Write("Введите число M: ");
+int m = int.Parse(ReadLine());
+Write("Введите число N: ");
+int n = int.Parse(ReadLine());
+
+AkkermanFunction(m,n);
+
+void AkkermanFunction(int m, int n)
+{
+    Write(Akkerman(m, n)); 
+}
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
